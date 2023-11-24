6f3b92837f15f28f0cdc825a83f9f9085371ac04
장영진 <44837049+jyoungjin@users.noreply.github.com>
2023-11-24 15:52:28 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 128 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+        bw.write(String.valueOf(n * m - 1));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
