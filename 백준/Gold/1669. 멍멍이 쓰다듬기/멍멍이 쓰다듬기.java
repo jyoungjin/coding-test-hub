5286b9503f2de633b2600cf09bf4bd479bdf5ac1
장영진 <44837049+jyoungjin@users.noreply.github.com>
2023-12-15 23:04:48 +0900

    [Gold V] Title: 멍멍이 쓰다듬기, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Gold/1669. 멍멍이 쓰다듬기/멍멍이 쓰다듬기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        if (x == y) {
+            System.out.println(0);
+            return;
+        }
+        for (long base = 1, gap = y-x; ; base++) {
+            long sum = base * base + base;
+            if (sum < gap) continue;
+            System.out.println(base * 2 + ( sum - base >= gap ? -1 : 0));
+            return;
+        }
+    }
+}
